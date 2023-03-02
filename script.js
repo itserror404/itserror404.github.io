304b437157005b82da5e8b3e7a0f1b3a41ec2b2d
Maimuna Zaheer <68902873+itserror404@users.noreply.github.com>
2023-03-02 16:46:29 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 
 var dt = new Date();
 document.getElementById('date-time').innerHTML=dt;
+
+// Toggle menu on small screens
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('clicked');
+});
+
+
